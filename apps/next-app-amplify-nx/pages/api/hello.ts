a5c29843b1dbf5da53d050ff3cb688c0a377711b
Bruno Antunes <bmvantunes@users.noreply.github.com>
2021-11-13 17:29:21 +0000

    do you like this aws?

--- apps/next-app-amplify-nx/pages/api/hello.ts
+++ apps/next-app-amplify-nx/pages/api/hello.ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John yeahhhhh Doe' })
+  res.status(200).json({ name: 'John xz Doe' })
 }
